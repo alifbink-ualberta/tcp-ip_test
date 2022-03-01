7233642c3f177149fcb02c97d2b72c74d9f3d846
Alif Bin Khorshed Ahmed <alifbink@ualberta.ca>
2022-02-28 19:08:43 -0700

    Added further code.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tcp-ip_test
 Program to test and play around with TCP/IP.
+
+- https://www.youtube.com/watch?v=3QiPPX-KeSc&ab_channel=TechWithTim
--- main.py
+++ main.py
@@ -3,8 +3,28 @@
 import socket
 import threading
 
+HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
-Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        msg = conn.recv()
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = socket.accept()
+        thread = threading.Thread(target = handle_client, args = (conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+print("[STARTING] server is starting...")
+start()
