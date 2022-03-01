eb75d8d26d9fc3adaee93a7618ebafa2a2843423
Alif Bin Khorshed Ahmed <alifbink@ualberta.ca>
2022-02-28 19:40:00 -0700

    Added comments and a condition.

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
-# This is a comment
-
 import socket
 import threading
 
 HEADER = 64
 PORT = 5050
+# Gets the local ipv4 address.
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
+# Creates socket and includes the family of addresses and the type of data stream.
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Binds the server and port.
 server.bind(ADDR)
 
 def handle_client(conn, addr):
@@ -19,23 +20,26 @@ def handle_client(conn, addr):
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
+        # First connection made gives a black message, so further code is implemented only if the message is not black.
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
 
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
 
-        print(f"[{addr}] {msg}")
+            print(f"[{addr}] {msg}")
     conn.close()
 
+# Starts the connection and handling of client. In threads to avoid consecutive handling of clients instead of simultaneous.
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, addr = socket.accept()
+        conn, addr = server.accept()
         thread = threading.Thread(target = handle_client, args = (conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 print("[STARTING] server is starting...")
 start()
